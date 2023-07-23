15c77e462e54b9cdccc21af08b920dd67ef77af0
melontini <104443436+melontini@users.noreply.github.com>
2023-07-23 12:26:10 +0700

    loose method

--- dark-matter-content/src/main/java/me/melontini/dark_matter/content/mixin/item_group_builder/ItemGroupMixin.java
+++ dark-matter-content/src/main/java/me/melontini/dark_matter/content/mixin/item_group_builder/ItemGroupMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 public class ItemGroupMixin {
 
     @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemGroup$EntryCollector;accept(Lnet/minecraft/resource/featuretoggle/FeatureSet;Lnet/minecraft/item/ItemGroup$Entries;Z)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT, method = "updateEntries")
-    private void cracker_util$injectEntries(FeatureSet enabledFeatures, boolean operatorEnabled, CallbackInfo ci, ItemGroup.EntriesImpl entriesImpl) {
+    private void dark_matter$injectEntries(FeatureSet enabledFeatures, boolean operatorEnabled, CallbackInfo ci, ItemGroup.EntriesImpl entriesImpl) {
         if (ItemGroupHelper.INJECTED_GROUPS.containsKey((ItemGroup) (Object) this)) {
             for (ItemGroupHelper.InjectEntries injectEntries : ItemGroupHelper.INJECTED_GROUPS.get((ItemGroup) (Object) this)) {
                 injectEntries.inject(enabledFeatures, operatorEnabled, entriesImpl);
