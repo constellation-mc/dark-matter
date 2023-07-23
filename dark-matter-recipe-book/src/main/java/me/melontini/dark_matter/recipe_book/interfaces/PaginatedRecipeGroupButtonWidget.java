2af4bc56a047172753fbcea6117ff5a7243992ba
melontini <104443436+melontini@users.noreply.github.com>
2023-07-23 11:09:34 +0700

    Deprecate non-prefixed methods in PaginatedRecipe*Widget.

--- dark-matter-recipe-book/src/main/java/me/melontini/dark_matter/recipe_book/interfaces/PaginatedRecipeGroupButtonWidget.java
+++ dark-matter-recipe-book/src/main/java/me/melontini/dark_matter/recipe_book/interfaces/PaginatedRecipeGroupButtonWidget.java
@@ -7,10 +7,20 @@ import org.jetbrains.annotations.ApiStatus;
 @Environment(EnvType.CLIENT)
 @ApiStatus.Experimental
 public interface PaginatedRecipeGroupButtonWidget {
+    default int dm$getPage() {
+        throw new IllegalStateException("Interface not implemented");
+    }
+
+    default void dm$setPage(int page) {
+        throw new IllegalStateException("Interface not implemented");
+    }
+
+    @Deprecated
     default int getPage() {
         throw new IllegalStateException("Interface not implemented");
     }
 
+    @Deprecated
     default void setPage(int page) {
         throw new IllegalStateException("Interface not implemented");
     }
