0b0371cbc397d8cd48d5c011e485b8065f4af432
melontini <104443436+melontini@users.noreply.github.com>
2023-08-09 21:57:42 +0700

    Prefer ItemGroupEvents if loaded. True for most instances. Don't require `dark_matter$injectEntries` in ItemGroupMixin

--- dark-matter-content/src/main/java/me/melontini/dark_matter/content/mixin/item_group_builder/ItemGroupMixin.java
+++ dark-matter-content/src/main/java/me/melontini/dark_matter/content/mixin/item_group_builder/ItemGroupMixin.java
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 @Mixin(ItemGroup.class)
 public class ItemGroupMixin {
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemGroup$EntryCollector;accept(Lnet/minecraft/resource/featuretoggle/FeatureSet;Lnet/minecraft/item/ItemGroup$Entries;Z)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT, method = "updateEntries")
+    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemGroup$EntryCollector;accept(Lnet/minecraft/resource/featuretoggle/FeatureSet;Lnet/minecraft/item/ItemGroup$Entries;Z)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT, method = "updateEntries", require = 0)
     private void dark_matter$injectEntries(FeatureSet enabledFeatures, boolean operatorEnabled, CallbackInfo ci, ItemGroup.EntriesImpl entriesImpl) {
         if (ItemGroupHelper.INJECTED_GROUPS.containsKey((ItemGroup) (Object) this)) {
             for (ItemGroupHelper.InjectEntries injectEntries : ItemGroupHelper.INJECTED_GROUPS.get((ItemGroup) (Object) this)) {
