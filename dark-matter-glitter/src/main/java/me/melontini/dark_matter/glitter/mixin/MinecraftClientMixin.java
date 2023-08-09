65f5be034624e584aab7eaa617784bff6c9c5e23
melontini <104443436+melontini@users.noreply.github.com>
2023-08-09 20:10:10 +0700

    don't wait for init in glitter

--- dark-matter-glitter/src/main/java/me/melontini/dark_matter/glitter/mixin/MinecraftClientMixin.java
+++ dark-matter-glitter/src/main/java/me/melontini/dark_matter/glitter/mixin/MinecraftClientMixin.java
@@ -6,7 +6,6 @@ import net.minecraft.resource.ResourcePackManager;
 import net.minecraft.server.SaveLoader;
 import net.minecraft.world.level.storage.LevelStorage;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -14,21 +13,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(MinecraftClient.class)
 public abstract class MinecraftClientMixin {
 
-    @Unique
-    private boolean dark_matter$init = false;
-
-    @Inject(method = "method_29338", at = @At("TAIL"))
-    private void dark_matter$init(CallbackInfo ci) {
-        this.dark_matter$init = true;
-    }
-
     @Inject(method = "tick", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/tutorial/TutorialManager;tick(Lnet/minecraft/client/world/ClientWorld;Lnet/minecraft/util/hit/HitResult;)V", shift = At.Shift.AFTER))
     private void dark_matter$tickParticles(CallbackInfo ci) {
-        if (this.dark_matter$init) ScreenParticleHelper.tickParticles();
+        ScreenParticleHelper.tickParticles();
     }
 
     @Inject(method = "startIntegratedServer", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/screen/LevelLoadingScreen;tick()V"))
     private void dark_matter$tickScreen(String levelName, LevelStorage.Session session, ResourcePackManager dataPackManager, SaveLoader saveLoader, CallbackInfo ci) {
-        if (this.dark_matter$init) ScreenParticleHelper.tickParticles();
+        ScreenParticleHelper.tickParticles();
     }
 }
