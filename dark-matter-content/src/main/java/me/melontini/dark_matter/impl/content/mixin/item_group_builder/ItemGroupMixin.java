2450090ee36bcf2fb197560b7674ae80f1382dc6
melontini <104443436+melontini@users.noreply.github.com>
2023-08-11 21:45:06 +0700

    Try injecting after NEW.

--- dark-matter-content/src/main/java/me/melontini/dark_matter/impl/content/mixin/item_group_builder/ItemGroupMixin.java
+++ dark-matter-content/src/main/java/me/melontini/dark_matter/impl/content/mixin/item_group_builder/ItemGroupMixin.java
@@ -1,5 +1,6 @@
 package me.melontini.dark_matter.impl.content.mixin.item_group_builder;
 
+import com.llamalad7.mixinextras.sugar.Local;
 import me.melontini.dark_matter.api.content.ItemGroupHelper;
 import me.melontini.dark_matter.impl.content.ItemGroupInjectionInternals;
 import net.minecraft.item.ItemGroup;
@@ -8,13 +9,12 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 @Mixin(ItemGroup.class)
 public class ItemGroupMixin {
 
-    @Inject(at = @At(value = "INVOKE", target = "Lnet/minecraft/item/ItemGroup$EntryCollector;accept(Lnet/minecraft/resource/featuretoggle/FeatureSet;Lnet/minecraft/item/ItemGroup$Entries;Z)V", shift = At.Shift.AFTER), locals = LocalCapture.CAPTURE_FAILSOFT, method = "updateEntries", require = 0)
-    private void dark_matter$injectEntries(FeatureSet enabledFeatures, boolean operatorEnabled, CallbackInfo ci, ItemGroup.EntriesImpl entriesImpl) {
+    @Inject(at = @At(value = "NEW", target = "(Lnet/minecraft/item/ItemGroup;Lnet/minecraft/resource/featuretoggle/FeatureSet;)Lnet/minecraft/item/ItemGroup$EntriesImpl;", shift = At.Shift.AFTER), method = "updateEntries", require = 0)
+    private void dark_matter$injectEntries(FeatureSet enabledFeatures, boolean operatorEnabled, CallbackInfo ci, @Local ItemGroup.EntriesImpl entriesImpl) {
         if (ItemGroupInjectionInternals.INJECTED_GROUPS.containsKey((ItemGroup) (Object) this)) {
             for (ItemGroupHelper.InjectEntries injectEntries : ItemGroupInjectionInternals.INJECTED_GROUPS.get((ItemGroup) (Object) this)) {
                 injectEntries.inject(enabledFeatures, operatorEnabled, entriesImpl);
