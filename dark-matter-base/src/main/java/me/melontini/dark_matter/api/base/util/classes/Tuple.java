56c08ed116eb1edc16bdaf7b8f8fcc27cb7857ae
melontini <104443436+melontini@users.noreply.github.com>
2023-08-25 00:16:07 +0700

    static `of` methods for Tuple and MutableTuple.

--- dark-matter-base/src/main/java/me/melontini/dark_matter/api/base/util/classes/Tuple.java
+++ dark-matter-base/src/main/java/me/melontini/dark_matter/api/base/util/classes/Tuple.java
@@ -1,6 +1,11 @@
 package me.melontini.dark_matter.api.base.util.classes;
 
 public record Tuple<L, R>(L left, R right) {
+
+    public static <L, R> Tuple<L, R> of(L left, R right) {
+        return new Tuple<>(left, right);
+    }
+
     @Override
     public String toString() {
         return "Tuple{" +
