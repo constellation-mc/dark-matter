8fa72b91d8ddaf6a4371b8e1ea80ecfb9bf284e5
melontini <104443436+melontini@users.noreply.github.com>
2023-08-26 01:40:20 +0700

    Revert "Patch the broken type ourselves."
    This reverts commit 35482717db06f31fb1675b805c0d574fb1ccc470.

--- dark-matter-glitter/src/main/java/me/melontini/dark_matter/impl/glitter/mixin/VanillaParticleMixin.java
+++
@@ -1,8 +0,0 @@
-package me.melontini.dark_matter.impl.glitter.mixin;
-
-import me.melontini.dark_matter.impl.glitter.particles.VanillaParticle;
-import org.spongepowered.asm.mixin.Mixin;
-
-@Mixin(VanillaParticle.class)
-public class VanillaParticleMixin {
-}
