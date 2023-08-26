58718b1c9fd23a7cf29731d4266c4d25165935a5
melontini <104443436+melontini@users.noreply.github.com>
2023-08-26 20:27:35 +0700

    Added a view to ItemGroupInjectionInternals. Also switched to LinkedHashMap.

--- dark-matter-content/src/main/java/me/melontini/dark_matter/impl/content/ItemGroupInjectionInternals.java
+++ dark-matter-content/src/main/java/me/melontini/dark_matter/impl/content/ItemGroupInjectionInternals.java
@@ -4,17 +4,17 @@ import me.melontini.dark_matter.api.content.ItemGroupHelper;
 import net.minecraft.item.ItemGroup;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
+import java.util.*;
 
 @ApiStatus.Internal
 public class ItemGroupInjectionInternals {
+
     private ItemGroupInjectionInternals() {
         throw new UnsupportedOperationException();
     }
-    public static final Map<ItemGroup, List<ItemGroupHelper.InjectEntries>> INJECTED_GROUPS = new ConcurrentHashMap<>();
+
+    private static final Map<ItemGroup, List<ItemGroupHelper.InjectEntries>> INJECTED_GROUPS = new LinkedHashMap<>();
+    private static final Map<ItemGroup, List<ItemGroupHelper.InjectEntries>> VIEW = Collections.unmodifiableMap(INJECTED_GROUPS);
 
     public static void addItemGroupInjection(ItemGroup group, ItemGroupHelper.InjectEntries injectEntries) {
         var list = INJECTED_GROUPS.computeIfAbsent(group, group1 -> new ArrayList<>());
@@ -24,4 +24,7 @@ public class ItemGroupInjectionInternals {
         }
     }
 
+    public static Map<ItemGroup, List<ItemGroupHelper.InjectEntries>> getView() {
+        return VIEW;
+    }
 }
