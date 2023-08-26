364810f7494987b1debd77e5210de6b6e6552b75
melontini <104443436+melontini@users.noreply.github.com>
2023-08-26 20:33:47 +0700

    Use a set instead of a list.

--- dark-matter-content/src/main/java/me/melontini/dark_matter/impl/content/ItemGroupInjectionInternals.java
+++ dark-matter-content/src/main/java/me/melontini/dark_matter/impl/content/ItemGroupInjectionInternals.java
@@ -1,5 +1,6 @@
 package me.melontini.dark_matter.impl.content;
 
+import me.melontini.dark_matter.api.base.util.MakeSure;
 import me.melontini.dark_matter.api.content.ItemGroupHelper;
 import net.minecraft.item.ItemGroup;
 import org.jetbrains.annotations.ApiStatus;
@@ -13,17 +14,14 @@ public class ItemGroupInjectionInternals {
         throw new UnsupportedOperationException();
     }
 
-    private static final Map<ItemGroup, List<ItemGroupHelper.InjectEntries>> INJECTED_GROUPS = new LinkedHashMap<>();
+    private static final Map<ItemGroup, Set<ItemGroupHelper.InjectEntries>> INJECTED_GROUPS = new LinkedHashMap<>();
 
     public static void addItemGroupInjection(ItemGroup group, ItemGroupHelper.InjectEntries injectEntries) {
-        var list = INJECTED_GROUPS.computeIfAbsent(group, group1 -> new ArrayList<>());
-
-        if (!list.contains(injectEntries)) {
-            list.add(injectEntries);
-        }
+        MakeSure.notNulls(group, injectEntries);
+        INJECTED_GROUPS.computeIfAbsent(group, group1 -> new LinkedHashSet<>()).add(injectEntries);
     }
 
-    public static Optional<List<ItemGroupHelper.InjectEntries>> getItemGroupInjections(ItemGroup group) {
+    public static Optional<Set<ItemGroupHelper.InjectEntries>> getItemGroupInjections(ItemGroup group) {
         return INJECTED_GROUPS.containsKey(group) ? Optional.of(INJECTED_GROUPS.get(group)) : Optional.empty();
     }
 }
