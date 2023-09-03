8439ca8d0b5181870525a278cf0a6dd5a1e47fbb
melontini <104443436+melontini@users.noreply.github.com>
2023-09-03 19:22:10 +0700

    Don't store Mixpanel handlers.

--- dark-matter-analytics-mixpanel/src/main/java/me/melontini/dark_matter/impl/analytics/mixpanel/MixpanelAnalyticsInternals.java
+++ dark-matter-analytics-mixpanel/src/main/java/me/melontini/dark_matter/impl/analytics/mixpanel/MixpanelAnalyticsInternals.java
@@ -5,16 +5,12 @@ import me.melontini.dark_matter.api.analytics.mixpanel.MixpanelHandler;
 import me.melontini.dark_matter.api.base.util.MakeSure;
 import org.jetbrains.annotations.ApiStatus;
 
-import java.util.Map;
-import java.util.concurrent.ConcurrentHashMap;
-
 @ApiStatus.Internal
 public class MixpanelAnalyticsInternals {
-    private static final Map<String, MixpanelHandler> MESSAGE_HANDLERS = new ConcurrentHashMap<>();
 
     public static MixpanelHandler init(Analytics analytics, String token, boolean eu) {
         MakeSure.notEmpty(token, "Invalid token provided! (null/empty)");
-        return MESSAGE_HANDLERS.computeIfAbsent(token, k -> new MixpanelHandler(analytics, new MixpanelAPI(eu, token)));
+        return new MixpanelHandler(analytics, new MixpanelAPI(eu, token));
     }
 
 }
