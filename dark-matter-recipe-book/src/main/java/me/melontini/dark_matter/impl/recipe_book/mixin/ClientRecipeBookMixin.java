6790aa8832242d6bb3b5c18c8165962e474918ae
melontini <104443436+melontini@users.noreply.github.com>
2023-09-10 16:04:27 +0700

    Use a registry manager provided by the handler.

--- dark-matter-recipe-book/src/main/java/me/melontini/dark_matter/impl/recipe_book/mixin/ClientRecipeBookMixin.java
+++ dark-matter-recipe-book/src/main/java/me/melontini/dark_matter/impl/recipe_book/mixin/ClientRecipeBookMixin.java
@@ -1,41 +1,31 @@
 package me.melontini.dark_matter.impl.recipe_book.mixin;
 
 import me.melontini.dark_matter.impl.recipe_book.RecipeBookInternals;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.network.ClientPlayNetworkHandler;
 import net.minecraft.client.recipebook.ClientRecipeBook;
 import net.minecraft.client.recipebook.RecipeBookGroup;
 import net.minecraft.recipe.Recipe;
 import net.minecraft.registry.DynamicRegistryManager;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import java.util.Optional;
 import java.util.function.BiFunction;
 
 @Mixin(ClientRecipeBook.class)
 public class ClientRecipeBookMixin {
 
-    @Unique
-    private static DynamicRegistryManager dark_matter$currentDynamicRegistryManager;
-
-    @Inject(at = @At("HEAD"), method = "reload")
-    private void dark_matter$reloadHead(Iterable<Recipe<?>> recipes, DynamicRegistryManager registryManager, CallbackInfo ci) {
-        dark_matter$currentDynamicRegistryManager = registryManager;
-    }
-
-    @Inject(at = @At("TAIL"), method = "reload")
-    private void dark_matter$reloadTail(Iterable<Recipe<?>> recipes, DynamicRegistryManager registryManager, CallbackInfo ci) {
-        dark_matter$currentDynamicRegistryManager = null;
-    }
-
     @Inject(at = @At("HEAD"), method = "getGroupForRecipe", cancellable = true)
     private static void dark_matter$getGroupForRecipe(Recipe<?> recipe, CallbackInfoReturnable<RecipeBookGroup> cir) {
+        var rm = Optional.ofNullable(MinecraftClient.getInstance().getNetworkHandler())
+                .map(ClientPlayNetworkHandler::getRegistryManager).orElse(null);
         RecipeBookInternals.getLookups(recipe.getType()).map(functions -> {
             RecipeBookGroup result;
             for (BiFunction<Recipe<?>, DynamicRegistryManager, RecipeBookGroup> function : functions) {
-                if ((result = function.apply(recipe, dark_matter$currentDynamicRegistryManager)) != null) {
+                if ((result = function.apply(recipe, rm)) != null) {
                     return result;
                 }
             }
