ba761f8c80bd72dd6be59261c675dba6829a8b7d
melontini <104443436+melontini@users.noreply.github.com>
2023-09-20 22:13:03 +0700

    Pseudo docs

--- dark-matter-config/src/main/java/me/melontini/dark_matter/api/config/ConfigBuilder.java
+++ dark-matter-config/src/main/java/me/melontini/dark_matter/api/config/ConfigBuilder.java
@@ -11,6 +11,10 @@ import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
+/**
+ * All parameters outside {@link ConfigBuilder#create(Class, ModContainer, String)} are optional.
+ * @param <T> your desired config class
+ */
 @ApiStatus.NonExtendable
 public interface ConfigBuilder<T> {
 
@@ -18,18 +22,50 @@ public interface ConfigBuilder<T> {
         return new ConfigBuilderImpl<>(cls, mod, name);
     }
 
+    /**
+     * The constructor used to create an instance of your config class.
+     * <p>
+     * Will fall back to reflection if not provided.
+     */
     ConfigBuilder<T> constructor(Supplier<T> ctx);
 
+    /**
+     * Allows you to register new fix-ups,
+     * which will be used to update the {@link com.google.gson.JsonObject} before parsing.
+     * <p>
+     * The entrypoint for this is {@code {modid}:config-fixups-{config}}
+     */
     ConfigBuilder<T> fixups(Consumer<FixupsBuilder> fixups);
 
+    /**
+     * Allows you to register redirects,
+     * which will be used to redirect old options to new ones.
+     * For example, if {@code enableOption} moved to {@code option.enable}
+     * <p>
+     * The entrypoint for this is {@code {modid}:config-redirects-{config}}
+     */
     ConfigBuilder<T> redirects(Consumer<RedirectsBuilder> redirects);
 
+    /**
+     * The getter used to get option values using string names.
+     */
     ConfigBuilder<T> getter(Getter<T> getter);
 
+    /**
+     * The setter used to set option values using string names.
+     */
     ConfigBuilder<T> setter(Setter<T> setter);
 
+    /**
+     * The Gson instance used to parse the config file.
+     */
     ConfigBuilder<T> gson(Gson gson);
 
+    /**
+     * Allows you to register new processors, which can be used to force set a value if some conditions are bet.
+     * <p>
+     * The entrypoint for this is {@code {modid}:config-processors-{config}}
+     */
     ConfigBuilder<T> processors(Consumer<OptionProcessorRegistry<T>> consumer);
 
     ConfigBuilder<T> scanner(ConfigClassScanner scanner);
