fed622a88fa59eda247fb57b4ddc47d68567fa49
melontini <104443436+melontini@users.noreply.github.com>
2023-10-16 20:14:47 +0700

    Remove Lambda field accessors

--- dark-matter-base/src/main/java/me/melontini/dark_matter/api/base/reflect/MiscReflection.java
+++ dark-matter-base/src/main/java/me/melontini/dark_matter/api/base/reflect/MiscReflection.java
@@ -8,28 +8,8 @@ import java.lang.invoke.MethodHandles;
 import java.lang.invoke.VarHandle;
 import java.lang.reflect.Field;
 import java.security.ProtectionDomain;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class MiscReflection {
-
-    public static Function<Object, Object> createGetter(Field field, MethodHandles.Lookup lookup) {
-        return MiscReflectionInternals.createGetter(field, lookup);
-    }
-
-    public static Supplier<Object> createStaticGetter(Field field, MethodHandles.Lookup lookup) {
-        return MiscReflectionInternals.createStaticGetter(field, lookup);
-    }
-
-    public static BiConsumer<Object, Object> createSetter(Field field, MethodHandles.Lookup lookup) {
-        return MiscReflectionInternals.createSetter(field, lookup);
-    }
-
-    public static Consumer<Object> createStaticSetter(Field field, MethodHandles.Lookup lookup) {
-        return MiscReflectionInternals.createStaticSetter(field, lookup);
-    }
 
     //
     // no
--- dark-matter-base/src/main/java/me/melontini/dark_matter/impl/base/reflect/MiscReflectionInternals.java
+++ dark-matter-base/src/main/java/me/melontini/dark_matter/impl/base/reflect/MiscReflectionInternals.java
@@ -5,81 +5,15 @@ import me.melontini.dark_matter.api.base.util.classes.Lazy;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.lang.invoke.*;
+import java.lang.invoke.MethodHandle;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodType;
+import java.lang.invoke.VarHandle;
 import java.lang.reflect.Field;
 import java.lang.reflect.Modifier;
 import java.security.ProtectionDomain;
-import java.util.function.BiConsumer;
-import java.util.function.Consumer;
-import java.util.function.Function;
-import java.util.function.Supplier;
 
 public class MiscReflectionInternals {
-
-    public static Function<Object, Object> createGetter(Field field, MethodHandles.Lookup lookup) {
-        try {
-            MethodHandle getter = lookup.unreflectGetter(field);
-            CallSite site = LambdaMetafactory.metafactory(
-                    lookup,
-                    "apply",
-                    MethodType.methodType(Function.class, MethodHandle.class),
-                    MethodType.methodType(Object.class, Object.class),
-                    MethodHandles.exactInvoker(getter.type()),
-                    MethodType.methodType(getter.type().wrap().returnType(), field.getDeclaringClass()));
-            return (Function<Object, Object>) site.getTarget().invoke(getter);
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-
-    public static Supplier<Object> createStaticGetter(Field field, MethodHandles.Lookup lookup) {
-        try {
-            MethodHandle getter = lookup.unreflectGetter(field);
-            CallSite site = LambdaMetafactory.metafactory(
-                    lookup,
-                    "get",
-                    MethodType.methodType(Supplier.class, MethodHandle.class),
-                    MethodType.methodType(Object.class),
-                    MethodHandles.exactInvoker(getter.type()),
-                    MethodType.methodType(getter.type().wrap().returnType()));
-            return (Supplier<Object>) site.getTarget().invoke(getter);
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static BiConsumer<Object, Object> createSetter(Field field, MethodHandles.Lookup lookup) {
-        try {
-            MethodHandle setter = lookup.unreflectSetter(field);
-            CallSite site = LambdaMetafactory.metafactory(
-                    lookup,
-                    "accept",
-                    MethodType.methodType(BiConsumer.class, MethodHandle.class),
-                    MethodType.methodType(void.class, Object.class, Object.class),
-                    MethodHandles.exactInvoker(setter.type()),
-                    MethodType.methodType(void.class, field.getDeclaringClass(), setter.type().wrap().parameterType(1)));
-            return (BiConsumer<Object, Object>) site.getTarget().invoke(setter);
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static Consumer<Object> createStaticSetter(Field field, MethodHandles.Lookup lookup) {
-        try {
-            MethodHandle setter = lookup.unreflectSetter(field);
-            CallSite site = LambdaMetafactory.metafactory(
-                    lookup,
-                    "accept",
-                    MethodType.methodType(Consumer.class, MethodHandle.class),
-                    MethodType.methodType(void.class, Object.class),
-                    MethodHandles.exactInvoker(setter.type()),
-                    MethodType.methodType(void.class, setter.type().wrap().parameterType(0)));
-            return (Consumer<Object>) site.getTarget().invoke(setter);
-        } catch (Throwable e) {
-            throw new RuntimeException(e);
-        }
-    }
 
     private static final Lazy<MethodHandle> defineClass = Lazy.of(() -> () -> ReflectionInternals.trustedLookup().findVirtual(ClassLoader.class, "defineClass", MethodType.methodType(Class.class, String.class, byte[].class, int.class, int.class, ProtectionDomain.class)));
 
