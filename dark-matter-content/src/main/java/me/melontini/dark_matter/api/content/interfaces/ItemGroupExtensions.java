9e79ad124bc0ec74ba2da43201b3f1ad160187fa
melontini <104443436+melontini@users.noreply.github.com>
2024-01-04 16:57:11 +0700

    Remove deprecated methods

--- dark-matter-content/src/main/java/me/melontini/dark_matter/api/content/interfaces/ItemGroupExtensions.java
+++ dark-matter-content/src/main/java/me/melontini/dark_matter/api/content/interfaces/ItemGroupExtensions.java
@@ -25,21 +25,4 @@ public interface ItemGroupExtensions {
     default AnimatedItemGroup dm$getIconAnimation() {
         throw new IllegalStateException("Interface not implemented");
     }
-
-    @Environment(EnvType.CLIENT)
-    @Deprecated(forRemoval = true)
-    default boolean shouldAnimateIcon() {
-        return dm$shouldAnimateIcon();
-    }
-
-    @Deprecated(forRemoval = true)
-    default ItemGroup setIconAnimation(AnimatedItemGroup animation) {
-        return dm$setIconAnimation(animation);
-    }
-
-    @Deprecated(forRemoval = true)
-    @Environment(EnvType.CLIENT)
-    default AnimatedItemGroup getIconAnimation() {
-        return dm$getIconAnimation();
-    }
 }
