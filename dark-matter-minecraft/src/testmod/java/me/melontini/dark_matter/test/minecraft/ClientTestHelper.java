1b3e03bff298550e3f7e39084182897e586d9a10
melontini <104443436+melontini@users.noreply.github.com>
2024-04-09 22:25:46 +0700

    Ugh what is going on

--- dark-matter-minecraft/src/testmod/java/me/melontini/dark_matter/test/minecraft/ClientTestHelper.java
+++ dark-matter-minecraft/src/testmod/java/me/melontini/dark_matter/test/minecraft/ClientTestHelper.java
@@ -1,6 +1,7 @@
 package me.melontini.dark_matter.test.minecraft;
 
 import me.melontini.dark_matter.api.base.util.EntrypointRunner;
+import me.melontini.dark_matter.impl.base.DarkMatterLog;
 import me.melontini.dark_matter.impl.minecraft.util.test.DarkMatterClientTest;
 import net.fabricmc.api.ClientModInitializer;
 import net.minecraft.client.MinecraftClient;
@@ -14,6 +15,7 @@ public class ClientTestHelper implements ClientModInitializer {
     public void onInitializeClient() {
         var thread = new Thread(() -> {
             try {
+                DarkMatterLog.info("Started client test.");
                 waitForWorldTicks(200);
 
                 EntrypointRunner.run("dark-matter:client_test", DarkMatterClientTest.class, DarkMatterClientTest::onDarkMatterClientTest);
--- dark-matter-minecraft/src/testmod/java/me/melontini/dark_matter/test/minecraft/mixin/MinecraftClientMixin.java
+++ dark-matter-minecraft/src/testmod/java/me/melontini/dark_matter/test/minecraft/mixin/MinecraftClientMixin.java
@@ -1,8 +1,15 @@
 package me.melontini.dark_matter.test.minecraft.mixin;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.QuickPlay;
-import net.minecraft.client.RunArgs;
+import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.registry.RegistryKeys;
+import net.minecraft.resource.DataConfiguration;
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.GameMode;
+import net.minecraft.world.GameRules;
+import net.minecraft.world.gen.GeneratorOptions;
+import net.minecraft.world.gen.WorldPresets;
+import net.minecraft.world.level.LevelInfo;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -13,7 +20,17 @@ public class MinecraftClientMixin {
 
     @Inject(method = "method_53528", at = @At("HEAD"), cancellable = true)
     private void avoidQuickPlayArg(CallbackInfo ci) {
-        QuickPlay.startQuickPlay(MinecraftClient.getInstance(), new RunArgs.QuickPlay("", "dm_test_world", "", ""), null);
+        MinecraftClient client = MinecraftClient.getInstance();
+
+        if (!client.getLevelStorage().levelExists("dm_test_world")) {
+            client.createIntegratedServerLoader().createAndStart("dm_test_world",
+                    new LevelInfo("dm_test_world", GameMode.CREATIVE, false, Difficulty.EASY, true,
+                            new GameRules(), DataConfiguration.SAFE_MODE),
+                    new GeneratorOptions(0, true, false),
+                    registryManager -> registryManager.get(RegistryKeys.WORLD_PRESET).entryOf(WorldPresets.FLAT).value().createDimensionsRegistryHolder(), null);
+        } else {
+            client.createIntegratedServerLoader().start("dm_test_world", () -> client.setScreen(new TitleScreen()));
+        }
         ci.cancel();
     }
 }
