61ae517dbbecbc798465984d9e8752bc741294bf
melontini <104443436+melontini@users.noreply.github.com>
2024-07-15 22:38:32 +0700

    Use inheritance in Result.

--- dark-matter-base/src/main/java/me/melontini/dark_matter/api/base/util/Result.java
+++ dark-matter-base/src/main/java/me/melontini/dark_matter/api/base/util/Result.java
@@ -1,6 +1,9 @@
 package me.melontini.dark_matter.api.base.util;
 
-import org.jetbrains.annotations.ApiStatus;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Value;
 import org.jetbrains.annotations.Contract;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
@@ -9,69 +12,137 @@ import java.util.Optional;
 import java.util.function.Function;
 import java.util.function.Predicate;
 
-public record Result<V, E>(Optional<V> value, Optional<E> error) {
-
-    private static final Result<?, ?> EMPTY = new Result<>(Optional.empty(), Optional.empty());
-    public static <V, E> @NotNull Result<V, E> of(@Nullable V value, @Nullable E error) {
-        return value == null && error == null ? empty() : new Result<>(Optional.ofNullable(value), Optional.ofNullable(error));
-    }
+@SuppressWarnings("unchecked")
+public abstract sealed class Result<V, E> permits Result.Err, Result.Ok {
 
     @Contract("_ -> new")
     public static <V, E> @NotNull Result<V, E> error(@Nullable E error) {
-        return new Result<>(Optional.empty(), Optional.ofNullable(error));
+        return error == null ? empty() : new Err<>(error);
     }
 
     public static <V, E> @NotNull Result<V, E> ok(@Nullable V value) {
-        return value == null ? empty() : new Result<>(Optional.of(value), Optional.empty());
+        return value == null ? empty() : new Ok<>(value);
     }
 
     public static <V, E> @NotNull Result<V, E> empty() {
-        return (Result<V, E>) EMPTY;
+        return (Result<V, E>) Ok.EMPTY;
     }
 
-    public <A> @NotNull Result<A, E> mapVal(Function<? super V, ? extends A> mapper) {
-        if (value().isEmpty()) return (Result<A, E>) this;
-        return new Result<>(value().map(mapper), error());
-    }
-
-    public <X> @NotNull Result<V, X> mapErr(Function<? super E, ? extends X> mapper) {
-        if (error().isEmpty()) return (Result<V, X>) this;
-        return new Result<>(value(), error().map(mapper));
-    }
-
+    public abstract <A> @NotNull Result<A, E> mapVal(Function<? super V, ? extends A> mapper);
+    public abstract <X> @NotNull Result<V, X> mapErr(Function<? super E, ? extends X> mapper);
     public <A, X> @NotNull Result<A, X> map(Function<? super V, ? extends A> valMapper, Function<? super E, ? extends X> errMapper) {
-        if (value().isEmpty() && error().isEmpty()) return empty();
-        return new Result<>(value().map(valMapper), error().map(errMapper));
+        return this.<A>mapVal(valMapper).mapErr(errMapper);
     }
 
-    public <A> @NotNull Result<A, E> flatmapVal(Function<? super V, ? extends Result<? extends A, E>> mapper) {
-        if (value().isEmpty()) return (Result<A, E>) this;
-        return MakeSure.notNull((Result<A, E>) mapper.apply(value().get()));
-    }
+    public abstract <A> @NotNull Result<A, E> flatmapVal(Function<? super V, ? extends Result<? extends A, E>> mapper);
+    public abstract <X> @NotNull Result<V, X> flatmapErr(Function<? super E, ? extends Result<V, ? extends X>> mapper);
 
-    public <X> @NotNull Result<V, X> flatmapErr(Function<? super E, ? extends Result<V, ? extends X>> mapper) {
-        if (error().isEmpty()) return (Result<V, X>) this;
-        return MakeSure.notNull((Result<V, X>) mapper.apply(error().get()));
-    }
-
-    public @NotNull Result<V, E> filterVal(Predicate<? super V> predicate) {
-        if (value().isEmpty()) return this;
-        return new Result<>(value().filter(predicate), error());
-    }
-
-    public @NotNull Result<V, E> filterErr(Predicate<? super E> predicate) {
-        if (error().isEmpty()) return this;
-        return new Result<>(value(), error().filter(predicate));
-    }
-
+    public abstract @NotNull Result<V, E> filterVal(Predicate<? super V> predicate);
+    public abstract @NotNull Result<V, E> filterErr(Predicate<? super E> predicate);
     public @NotNull Result<V, E> filter(Predicate<? super V> valPredicate, Predicate<? super E> errPredicate) {
-        if (value().isEmpty() && error().isEmpty()) return empty();
-        return new Result<>(value().filter(valPredicate), error().filter(errPredicate));
+        return this.filterVal(valPredicate).filterErr(errPredicate);
     }
 
-    @ApiStatus.Experimental
-    public @NotNull Result<V, E> intern() {
-        if (this == EMPTY) return this;
-        return error().isEmpty() && value().isEmpty() ? empty() : this;
+    public abstract Optional<V> value();
+    public abstract Optional<E> error();
+
+    @Value @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @EqualsAndHashCode(callSuper = false)
+    private static class Ok<V, E> extends Result<V, E> {
+        public static Ok<?, ?> EMPTY = new Ok<>(null);
+
+        V value;
+
+        @Override
+        public @NotNull <A> Result<A, E> mapVal(Function<? super V, ? extends A> mapper) {
+            if (value == null) return (Result<A, E>) this;
+            return ok(mapper.apply(value));
+        }
+
+        @Override
+        public @NotNull <X> Result<V, X> mapErr(Function<? super E, ? extends X> mapper) {
+            return (Result<V, X>) this;
+        }
+
+        @Override
+        public @NotNull <A> Result<A, E> flatmapVal(Function<? super V, ? extends Result<? extends A, E>> mapper) {
+            if (value == null) return (Result<A, E>) this;
+            return MakeSure.notNull((Result<A, E>) mapper.apply(value));
+        }
+
+        @Override
+        public @NotNull <X> Result<V, X> flatmapErr(Function<? super E, ? extends Result<V, ? extends X>> mapper) {
+            return (Result<V, X>) this;
+        }
+
+        @Override
+        public @NotNull Result<V, E> filterVal(Predicate<? super V> predicate) {
+            if (value == null) return this;
+            return predicate.test(value) ? this : empty();
+        }
+
+        @Override
+        public @NotNull Result<V, E> filterErr(Predicate<? super E> predicate) {
+            return this;
+        }
+
+        @Override
+        public Optional<V> value() {
+            return Optional.ofNullable(value);
+        }
+
+        @Override
+        public Optional<E> error() {
+            return Optional.empty();
+        }
+    }
+
+    @Value @AllArgsConstructor(access = AccessLevel.PRIVATE)
+    @EqualsAndHashCode(callSuper = false)
+    private static class Err<V, E> extends Result<V, E> {
+        E error;
+
+        @Override
+        public @NotNull <A> Result<A, E> mapVal(Function<? super V, ? extends A> mapper) {
+            return (Result<A, E>) this;
+        }
+
+        @Override
+        public @NotNull <X> Result<V, X> mapErr(Function<? super E, ? extends X> mapper) {
+            if (error == null) return (Result<V, X>) this;
+            return error(mapper.apply(error));
+        }
+
+        @Override
+        public @NotNull <A> Result<A, E> flatmapVal(Function<? super V, ? extends Result<? extends A, E>> mapper) {
+            return (Result<A, E>) this;
+        }
+
+        @Override
+        public @NotNull <X> Result<V, X> flatmapErr(Function<? super E, ? extends Result<V, ? extends X>> mapper) {
+            if (error == null) return (Result<V, X>) this;
+            return MakeSure.notNull((Result<V, X>) mapper.apply(error));
+        }
+
+        @Override
+        public @NotNull Result<V, E> filterVal(Predicate<? super V> predicate) {
+            return this;
+        }
+
+        @Override
+        public @NotNull Result<V, E> filterErr(Predicate<? super E> predicate) {
+            if (error == null) return this;
+            return predicate.test(error) ? this : empty();
+        }
+
+        @Override
+        public Optional<V> value() {
+            return Optional.empty();
+        }
+
+        @Override
+        public Optional<E> error() {
+            return Optional.ofNullable(error);
+        }
     }
 }
