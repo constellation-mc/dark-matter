7d8a93fed16379b706fdfa5b5c90ebaec89e746f
melontini <104443436+melontini@users.noreply.github.com>
2024-08-02 16:27:00 +0700

    Guess what I forgot to apply again?

--- dark-matter-base/src/test/java/me/melontini/dark_matter/test/base/reflect/UnsafeUtilsTest.java
+++ dark-matter-base/src/test/java/me/melontini/dark_matter/test/base/reflect/UnsafeUtilsTest.java
@@ -54,28 +54,36 @@ public class UnsafeUtilsTest {
     String cls = UnsafeUtilsTest.class.getName().replace('.', '/') + "Dummy";
 
     ClassNode node = new ClassNode();
-    node.visit(Opcodes.V17, Opcodes.ACC_PUBLIC,
-            cls,
-            null, Type.getInternalName(Object.class), null);
+    node.visit(
+        Opcodes.V17, Opcodes.ACC_PUBLIC, cls, null, Type.getInternalName(Object.class), null);
 
-    InstructionAdapter adapter = new InstructionAdapter(node.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null));
+    InstructionAdapter adapter =
+        new InstructionAdapter(node.visitMethod(Opcodes.ACC_PUBLIC, "<init>", "()V", null, null));
     adapter.load(0, Type.getType(Object.class));
     adapter.invokespecial("java/lang/Object", "<init>", "()V", false);
     adapter.anew(Type.getType(UnsupportedOperationException.class));
     adapter.dup();
     adapter.aconst("This test constructor must throw an error!");
-    adapter.invokespecial(Type.getInternalName(UnsupportedOperationException.class), "<init>", Type.getMethodDescriptor(Type.VOID_TYPE, Type.getType(String.class)), false);
+    adapter.invokespecial(
+        Type.getInternalName(UnsupportedOperationException.class),
+        "<init>",
+        Type.getMethodDescriptor(Type.VOID_TYPE, Type.getType(String.class)),
+        false);
     adapter.athrow();
 
     ClassWriter writer = new ClassWriter(ClassWriter.COMPUTE_FRAMES | ClassWriter.COMPUTE_MAXS);
     node.accept(writer);
-    Class<?> defined = UnsafeUtils.defineClass(this.getClass().getClassLoader(), cls.replace('/', '.'), writer.toByteArray(), this.getClass().getProtectionDomain());
+    Class<?> defined = UnsafeUtils.defineClass(
+        this.getClass().getClassLoader(),
+        cls.replace('/', '.'),
+        writer.toByteArray(),
+        this.getClass().getProtectionDomain());
 
     Assertions.assertThatThrownBy(() -> defined.getConstructors()[0].newInstance())
-            .isInstanceOf(InvocationTargetException.class)
-            .extracting(Throwable::getCause, Assertions.as(InstanceOfAssertFactories.THROWABLE))
-            .isInstanceOf(UnsupportedOperationException.class)
-            .hasMessage("This test constructor must throw an error!");
+        .isInstanceOf(InvocationTargetException.class)
+        .extracting(Throwable::getCause, Assertions.as(InstanceOfAssertFactories.THROWABLE))
+        .isInstanceOf(UnsupportedOperationException.class)
+        .hasMessage("This test constructor must throw an error!");
   }
 
   public static class TestClassWithObject {
